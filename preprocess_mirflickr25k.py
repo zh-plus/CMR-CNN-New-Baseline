82c052802c297cad1a9600c0d533622ebbfe99f2
Hao Zheng <zhenghaosustc@gmail.com>
2020-09-07 19:49:26 +0800

    Done NUS-WIDE data pre-processing.

--- preprocess_mirflickr25k.py
+++ preprocess_mirflickr25k.py
@@ -6,10 +6,42 @@ import numpy as np
 
 from tqdm import tqdm
 
+mirflickr25k_data_dir = './data/mirflickr25k'
+
+
+def get_mirflickr_tag_map():
+    tag2id = dict()
+    id2tag = dict()
+
+    with open(mirflickr25k_data_dir + '/mirflickr25k/doc/common_tags.txt') as f:
+        tag_names = [l.split()[0] for l in f.readlines()]
+    for i, name in enumerate(tag_names):
+        tag2id[name] = i
+        id2tag[i] = name
+
+    print(f'text dimension: {len(tag2id)}')
+
+    return tag2id, id2tag
+
+
+def get_mirflickr_label_map():
+    label2id = dict()
+    id2label = dict()
+
+    for root, _, filenames in os.walk(mirflickr25k_data_dir + '/mirflickr25k_annotations_v080/remove_r1'):
+        for i, filename in enumerate(filenames):
+            label = filename[:-4]
+            label2id[label] = i
+            id2label[i] = label
+
+    print(f'label dimension: {len(label2id)}')
+
+    return label2id, id2label
+
 
 def remove_r1_and_readme():
-    origin_path = './mirflickr25k_annotations_v080'
-    processed_path = 'mirflickr25k_annotations_v080/remove_r1'
+    origin_path = mirflickr25k_data_dir + '/mirflickr25k_annotations_v080'
+    processed_path = mirflickr25k_data_dir + '/mirflickr25k_annotations_v080/remove_r1'
 
     os.makedirs(processed_path, exist_ok=True)
 
@@ -23,42 +55,12 @@ def remove_r1_and_readme():
             shutil.copy2(origin, processed_path)
 
 
-def get_tag_map():
-    tag2id = dict()
-    id2tag = dict()
-
-    with open('mirflickr25k/doc/common_tags.txt') as f:
-        tag_names = [l.split()[0] for l in f.readlines()]
-    for i, name in enumerate(tag_names):
-        tag2id[name] = i
-        id2tag[i] = name
-
-    print(f'text dimension: {len(tag2id)}')
-
-    return tag2id, id2tag
-
-
-def get_label_map():
-    label2id = dict()
-    id2label = dict()
-
-    for root, _, filenames in os.walk('./mirflickr25k_annotations_v080/remove_r1'):
-        for i, filename in enumerate(filenames):
-            label = filename[:-4]
-            label2id[label] = i
-            id2label[i] = label
-
-    print(f'label dimension: {len(label2id)}')
-
-    return label2id, id2label
-
-
 def process_tags():
-    tag2id, id2tag = get_tag_map()
+    tag2id, id2tag = get_mirflickr_tag_map()
     text_dim = len(tag2id)
 
-    for root, _, filenames in os.walk('mirflickr25k/meta/tags'):
-        texts = np.zeros((len(filenames), text_dim), dtype=float)  # BOW
+    for root, _, filenames in os.walk(mirflickr25k_data_dir + '/mirflickr25k/meta/tags'):
+        texts = np.zeros((len(filenames), text_dim), dtype=np.float32)  # BOW
         filenames.sort(key=lambda x: int(x[4:-4]))
 
         for i, filename in enumerate(filenames):
@@ -71,15 +73,15 @@ def process_tags():
                     texts[i][index] = 1
 
     print(f'Saving texts(tags) with shape: {texts.shape}')
-    np.save('texts.npy', texts)
+    np.save(mirflickr25k_data_dir + '/texts.npy', texts)
 
 
 def process_labels():
-    label2id, id2label = get_label_map()
+    label2id, id2label = get_mirflickr_label_map()
     label_dim = len(label2id)
 
     label_map = dict()
-    for root, _, files in os.walk('mirflickr25k_annotations_v080/remove_r1'):
+    for root, _, files in os.walk(mirflickr25k_data_dir + '/mirflickr25k_annotations_v080/remove_r1'):
         for file in files:
             with open(os.path.join(root, file)) as f:
                 label_map[file[:-4]] = set(map(int, f.readlines()))
@@ -90,7 +92,7 @@ def process_labels():
                 return l
 
     total_num = 25000
-    labels = np.zeros((total_num, label_dim), dtype=float)  # one-hot labels
+    labels = np.zeros((total_num, label_dim), dtype=np.float32)  # one-hot labels
     for i in range(1, total_num + 1):
         label = get_label(i)
         if label not in label2id.keys():
@@ -100,11 +102,11 @@ def process_labels():
         labels[i - 1][index] = 1  # start from 0 instead of 1
 
     print(f'Saving labels with shape: {labels.shape}')
-    np.save('labels.npy', labels)
+    np.save(mirflickr25k_data_dir + '/labels.npy', labels)
 
 
 def process_images():
-    image_folder = './mirflickr25k'
+    image_folder = mirflickr25k_data_dir + '/mirflickr25k'
     images = []
 
     filenames = [f for f in os.listdir(image_folder) if f.endswith('.jpg')]
@@ -115,37 +117,37 @@ def process_images():
 
     images = np.array(images, dtype=object)
 
-    print(f'Saving labels with shape: {images.shape}')
-    np.save('images.npy', images, allow_pickle=True)
+    print(f'Saving images with shape: {images.shape}')
+    np.save(mirflickr25k_data_dir + '/images.npy', images, allow_pickle=True)
 
 
 def clean():
-    data_root = '.'
-
-    texts = np.load(f'{data_root}/texts.npy')
-    labels = np.load(f'{data_root}/labels.npy')
-    images = np.load(f'{data_root}/images.npy', allow_pickle=True)
+    texts = np.load(f'{mirflickr25k_data_dir}/texts.npy')
+    labels = np.load(f'{mirflickr25k_data_dir}/labels.npy')
+    images = np.load(f'{mirflickr25k_data_dir}/images.npy', allow_pickle=True)
 
     # Remove elements that without text or label (text/label vector is all-zero vector)
     delete_indexes = [i for (i, (t, l)) in enumerate(zip(texts, labels)) if not any(t) or not any(l)]
+
+    print(f'Data left: {len(texts) - len(delete_indexes)}')
 
     # Get (20015, ...)
     texts = np.delete(texts, delete_indexes, axis=0)
     labels = np.delete(labels, delete_indexes, axis=0)
     images = np.delete(images, delete_indexes, axis=0)
 
-    np.save('texts.npy', texts)
-    np.save('labels.npy', labels)
-    np.save('images.npy', images, allow_pickle=True)
+    np.save(f'{mirflickr25k_data_dir}/texts.npy', texts)
+    np.save(f'{mirflickr25k_data_dir}/labels.npy', labels)
+    np.save(f'{mirflickr25k_data_dir}/images.npy', images, allow_pickle=True)
 
-    print('here')
+    print('Data clean done.')
 
 
 def main():
-    # remove_r1_and_readme()
-    # process_tags()
-    # process_labels()
-    # process_images()
+    remove_r1_and_readme()
+    process_tags()
+    process_labels()
+    process_images()
     clean()
 
 
